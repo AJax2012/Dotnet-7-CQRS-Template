7c21c9347f84d8b2c01c94c36ae75ab32e6fbefd
Adam Gardner <adga2012@gmail.com>
2024-04-03 21:18:53 -0600

    add mediatr to DI

--- src/Api/Loaders/DependencyInjection.cs
+++ src/Api/Loaders/DependencyInjection.cs
@@ -6,6 +6,11 @@ internal static class DependencyInjection
 {
     internal static IServiceCollection RegisterDependencies(this IServiceCollection services)
     {
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssemblyContaining<IApiMarker>();
+        });
+        
         return services;
     }
 }
