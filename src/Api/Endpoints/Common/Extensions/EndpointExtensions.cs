a0f999fac3d69f761d021067202e629c58d07828
Adam Gardner <adga2012@gmail.com>
2024-05-01 20:02:40 -0600

    add MapEndpoint and add example to comment

--- src/Api/Endpoints/Common/Extensions/EndpointExtensions.cs
+++ src/Api/Endpoints/Common/Extensions/EndpointExtensions.cs
@@ -5,7 +5,7 @@ using System.Reflection;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
-
+using Microsoft.AspNetCore.Routing;
 using SourceName.Api.Endpoints.Common.Contracts;
 using SourceName.Api.Endpoints.Common.Filters;
 
@@ -33,6 +33,12 @@ internal static class EndpointExtensions
     /// <summary>
     /// Used on an endpoint to add a request validation filter to the route handler.
     /// </summary>
+    /// <example>
+    /// <code>
+    /// app.MapGet("{id}", Handle)
+    ///    .WithRequestValidation&lt;SomeRequest&gt;();
+    /// </code>
+    /// </example>
     /// <param name="builder"><see cref="RouteHandlerBuilder"/>.</param>
     /// <typeparam name="TRequest"></typeparam>
     /// <returns></returns>
@@ -41,6 +47,25 @@ internal static class EndpointExtensions
             builder
                 .AddEndpointFilter<RequestValidationFilter<TRequest>>()
                 .ProducesValidationProblem();
+
+    /// <summary>
+    /// Maps an endpoint to a <see cref="RouteGroupBuilder"/> or an <see cref="IFeatureEndpoints"/> class.
+    /// </summary>
+    /// <example>
+    /// <code>
+    /// app.MapGroup("/api")
+    ///    .MapEndpoint&lt;SomeEndpoint&gt;();
+    /// </code>
+    /// </example>
+    /// <param name="app"><see cref="IEndpointRouteBuilder"/>.</param>
+    /// <typeparam name="TEndpoint">Endpoint class that implements <see cref="IEndpoint"/>.</typeparam>
+    /// <returns></returns>
+    internal static IEndpointRouteBuilder MapEndpoint<TEndpoint>(this IEndpointRouteBuilder app) 
+        where TEndpoint : IEndpoint
+    {
+        TEndpoint.Map(app);
+        return app;
+    }
 
     private static IEnumerable<TypeInfo> GetEndpointTypesFromContainingAssembly<TMarker>() =>
         typeof(TMarker).Assembly.DefinedTypes
