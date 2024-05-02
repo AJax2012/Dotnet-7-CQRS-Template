d2f53ca1884db88ad1ed5038a349f94f42cd7c28
Adam Gardner <adga2012@gmail.com>
2024-05-01 18:49:10 -0600

    Make FeatureEndpoints and refactor IEndpoint

---
+++ src/Api/Endpoints/Common/Contracts/IEndpoint.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Routing;
+
+namespace SourceName.Api.Endpoints.Common.Contracts;
+
+/// <summary>
+/// Marker interface for an Endpoint.
+/// </summary>
+public interface IEndpoint
+{
+	/// <summary>
+	/// Defines an endpoint.
+	/// </summary>
+	/// <param name="app"></param>
+	/// <returns></returns>
+	public static abstract IEndpointRouteBuilder Map(IEndpointRouteBuilder app);
+}
