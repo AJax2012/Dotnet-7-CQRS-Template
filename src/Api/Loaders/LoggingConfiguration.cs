818d248454cd8c569922b3c7658fc62f9cd6837e
Adam Gardner <adga2012@gmail.com>
2024-05-02 20:42:15 -0600

    Remove EnableFileLogger and use appsettings.json for logging configuration.

--- src/Api/Loaders/LoggingConfiguration.cs
+++ src/Api/Loaders/LoggingConfiguration.cs
@@ -1,10 +1,6 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Logging;
 using Serilog;
-using Serilog.Exceptions;
-using Serilog.Formatting.Json;
-using Serilog.Sinks.SystemConsole.Themes;
-using Throw;
 
 using ILogger = Serilog.ILogger;
 
@@ -15,20 +11,9 @@ internal static class LoggingConfiguration
     internal static ILogger AddLogging(this WebApplicationBuilder builder)
     {
         builder.Logging.ClearProviders();
-
-#if EnableFileLogger
-		var loggingFilePath = builder.Configuration.GetSection("LoggingFilePath").Value;
-		loggingFilePath.ThrowIfNull().IfNullOrWhiteSpace(_ => "LoggingFilePath",
-		    "When using file logger for serilog, LoggingFilePath must be set in appsettings or environment configuration");
-#endif
-
+        
         ILogger logger = new LoggerConfiguration()
-            .Enrich.WithExceptionDetails()
-            .Enrich.WithDemystifiedStackTraces()
-            .WriteTo.Console(theme: AnsiConsoleTheme.Code)
-#if EnableFileLogger
-            .WriteTo.File(new JsonFormatter(renderMessage: true), loggingFilePath, rollingInterval: RollingInterval.Day)
-#endif
+            .ReadFrom.Configuration(builder.Configuration)
             .CreateLogger();
 
         builder.Host.UseSerilog(logger);
